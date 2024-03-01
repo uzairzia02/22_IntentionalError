8f50362355737cdb646262819e6ff106bcc56815
uzairzia <uzair.governorsindh@gmail.com>
2024-03-01 17:52:52 +0500

    initial commit

---
+++ 22_IntentionalError.js
@@ -0,0 +1,7 @@
+"use strict";
+//creating an array
+let cricketers = ['Imam ul Haq', 'Fakhar Zaman', 'Babar Azam', 'M Rizwan'];
+// intentional error
+console.log(cricketers[4]);
+//correction
+console.log(cricketers);
---
+++ 22_IntentionalError.ts
@@ -0,0 +1,9 @@
+
+//creating an array
+let cricketers: string[]= ['Imam ul Haq', 'Fakhar Zaman', 'Babar Azam', 'M Rizwan'];
+
+// intentional error
+console.log(cricketers[4]);
+
+//correction
+console.log(cricketers);
